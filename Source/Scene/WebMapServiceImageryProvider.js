6b05f3c41b5c24ecf7de9c2a3b5b40577a32105b
zoran995 <zorankokeza95@gmail.com>
2020-07-05 16:55:12 +0200

    update wms 1.3 config for get feature info

--- Source/Scene/WebMapServiceImageryProvider.js
+++ Source/Scene/WebMapServiceImageryProvider.js
@@ -139,10 +139,16 @@ import UrlTemplateImageryProvider from './UrlTemplateImageryProvider.js';
 
         var pickFeatureParams = {
             query_layers: options.layers,
-            x: '{i}',
-            y: '{j}',
             info_format: '{format}'
         };
+        // use correct identifier based on version
+        if (parseFloat(resource.queryParameters.version) >= 1.3) {
+            pickFeatureParams.i = '{i}';
+            pickFeatureParams.j = '{j}';
+        } else {
+            pickFeatureParams.x = '{i}';
+            pickFeatureParams.y = '{j}';
+        }
         pickFeatureResource.setQueryParameters(pickFeatureParams, true);
 
         this._resource = resource;
